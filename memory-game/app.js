efaf82d31e0a681b5ae318ed5c0b9a5d5e09e75b
gdiego07 <d.garcia07@outlook.com>
2020-12-09 03:10:41 +0000

    dynamically added a static values in stats container

--- memory-game/app.js
+++ memory-game/app.js
@@ -14,7 +14,6 @@
     const cardImages = [...images, ...images];
 
     cardImages.forEach(image => {
-    console.log(image);
        const cardContainer = document.createElement("div");
        cardContainer.id = "card-container";
        const card = document.createElement('div');
@@ -23,9 +22,7 @@
        back.className = "back";
        const front = document.createElement('div');
        front.className = 'front'
-       front.style.backgroundImage = `url:(/images/${image})`;
-       back.style.backgroundImage = `url:(/images/back-side.png)`;
-    console.log(cardContainer);
+       front.style.backgroundImage = `url("${image}")`;
        cardContainer.append(card);
        card.append(back);
        card.append(front);
@@ -35,8 +32,35 @@
        main.append(gameContainer);
     }) 
  };
+
+ function displayStats() {
+//  <div id="stats">
+//    <div class="score"></div>
+//    <div class="timer"></div>
+//    <div class="lives"></div>
+//  </div>;
+let scoreTrack = "0";
+let countdown = "2:00";
+let health = "5";
+const score = document.createElement("div");
+score.className = "score";
+const timer = document.createElement("div");
+timer.className = "timer";
+const lives = document.createElement("div");
+lives.className = "lives"
+score.append(scoreTrack);
+timer.append(countdown);
+lives.append(health);
+const stats = document.querySelector("#stats");
+stats.append(score);
+stats.append(timer);
+stats.append(lives);
+
+
+ }; 
    function initalizeApp() {
       createGameCards(imagesArray);
+      displayStats();
  };
  
 
